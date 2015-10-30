19d4bb24c7cb7367f8949e532215d98b6ce96578
Lech Szymanski <lechszym@cs.otago.ac.nz>
2015-10-30 13:40:56 +1300

    Switched JS script to CSharp script

---
+++ Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,24 @@
+﻿/*
+	Created by: Lech Szymanski
+				lechszym@cs.otago.ac.nz
+				Oct 30, 2015			
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerClass : MonoBehaviour {
+
+	// Private variables (not visible in the Inspector panel)
+	// The speed of player movement
+	float speed = 10;
+	
+	// Update is called once per frame
+	void Update () {
+		// Player movement from input (it's a variable between -1 and 1) for
+		// degree of left or right movement
+		float movementInput = Input.GetAxis("Horizontal");
+		// Move the player object
+		transform.Translate( new Vector3(Time.deltaTime * speed * movementInput,0,0));	
+	}
+}
